3d99e4dd976814ec8f1f7bcb218da4a5b01be248
karthik990 <knag88@gmail.com>
2020-11-30 04:58:12 +0000

    dumpling: RR Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/rr_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    rr_dumpling-user \
+    rr_dumpling-userdebug \
+    rr_dumpling-eng
